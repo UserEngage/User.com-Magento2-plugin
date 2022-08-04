427a84b2247b0f3993b7a077830c5a33166c279f
szokart <szokart@s153.cyber-folks.pl>
2022-08-04 09:06:39 +0200

    checkout event

---
+++ Observer/Controller/ActionPredispatchCheckoutIndexIndex.php
@@ -0,0 +1,30 @@
+<?php                                                         
+
+namespace Usercom\Analytics\Observer\Controller;                        
+
+class ActionPredispatchCheckoutIndexIndex implements \Magento\Framework\Event\ObserverInterface                                
+{
+
+    protected $helper;
+
+    public function __construct(
+        \Usercom\Analytics\Helper\Data $helper
+    ){
+
+        $this->helper = $helper;
+    }
+
+    public function execute(
+        \Magento\Framework\Event\Observer $observer
+    ) {
+
+        if(!$this->helper->isModuleEnabled())
+            return;
+
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        $logger->info("checkout");
+
+    }
+}
