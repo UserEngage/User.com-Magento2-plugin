ec57f520f050505ab279191deb8d12df31685f6e
szokart <szokart@s153.cyber-folks.pl>
2022-08-20 21:54:10 +0200

    create frontend controller

---
+++ Controller/Order/Step2.php
@@ -0,0 +1,18 @@
+<?php
+namespace Usercom\Analytics\Controller\Order;
+
+class Step2 extends \Magento\Framework\App\Action\Action
+{
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context
+    ){
+		return parent::__construct($context);
+	}
+
+	public function execute(){
+            
+        echo "lol";
+
+	}
+}
