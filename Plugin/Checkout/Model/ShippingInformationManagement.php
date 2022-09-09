dc97a98cfb4a7a304a75d45fc28eb09b2e5bdbc7
szokart <szokart@s153.cyber-folks.pl>
2022-09-09 08:46:09 +0200

    remove order step 2 api, install plugin

---
+++ Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -0,0 +1,70 @@
+<?php                                                         
+
+namespace Usercom\Analytics\Plugin\Checkout\Model;                        
+
+class ShippingInformationManagement
+{
+    protected $helper;
+    protected $usercom; 
+    protected $cart;
+
+
+    public function __construct(
+        \Usercom\Analytics\Helper\Data $helper,
+        \Usercom\Analytics\Helper\Usercom $usercom,
+        \Magento\Checkout\Model\Cart $cart
+    ){
+        $this->helper = $helper;
+        $this->usercom = $usercom;
+        $this->cart = $cart;
+    }
+    /**
+     * @param \Magento\Checkout\Model\ShippingInformationManagement $subject
+     * @param $cartId
+     * @param \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        \Magento\Checkout\Model\ShippingInformationManagement $subject,
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    ) {
+        if( !$this->helper->isModuleEnabled() || !($usercomCustomerId = $this->usercom->getUsercomCustomerId()) )
+            return;
+
+        $shippingAddress = $addressInformation->getShippingAddress();
+        $data = array(
+            "first_name" =>  $shippingAddress->getFirstName(),
+            "last_name" => $shippingAddress->getLastName(),
+            "email" => $shippingAddress->getEmail(),
+            "phone_number" => $shippingAddress->getTelephone()
+        );
+
+        $this->usercom->updateCustomer($usercomCustomerId,$data);
+
+        $items = "";
+        foreach ($this->cart->getQuote()->getAllVisibleItems() as $item)
+            $items .= $item->getName().",";
+        $items = trim($items,","); 
+
+        $data = array(
+            "user_id" => $usercomCustomerId,
+            "name" => "order",
+            "timestamp" => time(),
+            "data" => array(
+                "step" => 2,
+                "first_name" =>  $shippingAddress->getFirstName(),
+                "last_name" => $shippingAddress->getLastName(),
+                "email" => $shippingAddress->getEmail(),
+                "phone_number" => $shippingAddress->getTelephone(),
+                "items" => $items,
+                "region" => $shippingAddress->getRegion(),
+                "street" => $shippingAddress->getStreet(),
+                "postcode" => $shippingAddress->getPostcode(),
+                "city" => $shippingAddress->getCity()
+            )
+        );
+
+        $this->usercom->createEvent($data);
+    }
+
+}
